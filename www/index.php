ec741d3bfc4e9a5b8b269fd77d8f02b06f4c69b1
giboow <philippe.gibert@gmail.com>
2012-11-05 16:24:20 +0100

    renaming

--- www/index.php
+++ www/index.php
@@ -4,68 +4,133 @@
 		<title>Fais toi plaiz</title>
 
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/style2.css">
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery.transit.js"></script>
-		<script type="text/javascript" src="js/main.js"></script>
+		<script type="text/javascript" src="http://jquery.offput.ca/js/jquery.timers.js"></script>
+		<script type="text/javascript" src="js/main2.js"></script>
 	</head>
 	<body>
 		<div id="site">
-			<div id="logo">
-				<div class="text">Fais toi plaiz!</div>
-			</div>
-
-			<div id="leftBlock">
-				<div class="content">
+			<header id="homeHeader">
+				<div id="logo">
+					<div class="imgLogo"></div>
 					<div class="firstStep">
-						<div class="text">Tu veux un compliment?</div>
-						<div class="img"></div>
+						<div class="separator"></div>
+						<div class="text">Besoin d'&ecirc;tre compliment&eacute;?</div>
+						<div class="text">Ou besoin de vannes qui tues pour tes potes?</div>
 					</div>
-					<div class="secondStep">
-						<div class="form">
-							<div class="text">
-								Comment t'appelles tu
+				</div>
+			</header>
+			<div id="content">
+				<div class="secondStep">
+					<div id="leftBlock" class="lightBg">
+						<div class="contentBlock">
+							<div class="imgBlock"></div>
+							<div class="title">
+								<div class="first">Fais-toi plaiz</div>
+								<div class="middle">
+									avec
+									<div class="separatorLeft"></div>
+									<div class="separatorRight"></div>
+								</div>
+								<div class="last">un compliment</div>
 							</div>
-							<form type="POST" action="getPhrase.php">
-								<input type="text" name="preif (blockClick) {
-			return;
-		}
-		blockClick = true;
-		var pos = block.position();
-		block.children(".content").children(".firstStep").hide();
-		$(depBlock).css('display', 'none');
-		$("#separatorBlock").hide();
-		block.transition({width: "100%"}, 1000, function(){
-			$(this).children(".content").children(".secondStep").show();
-		});
-		$("#logo").transition({opacity: 0}, 500);nom" />
-								<input type="submit" value="// Valider //" name="valider">
-							</form>
+							<div class="subTitle">
+								<div class="text">
+									T'es un peu triste?<br />
+									Remonte toi le moral en recevant<br />
+									un compliment!<br />
+								</div>
+								<div class="beginBt">//commencer\\</div>
+							</div>
 						</div>
 					</div>
-					<div class="thirdStep">
+					<div id="rightBlock" class="darkBg">
+						<div class="contentBlock">
+							<div class="imgBlock"></div>
+							<div class="title">
+								<div class="first">Fais-toi plaiz</div>
+								<div class="middle">
+									avec
+									<div class="separatorLeft"></div>
+									<div class="separatorRight"></div>
+								</div>
+								<div class="last">une vanne</div>
+							</div>
+							<div class="subTitle">
+								<div class="text">
+									T'as besoin de te d&eacute;fouler?<br />
+									Choisis ta cible et taille la avec<br />
+									une insulte bien sentie!
+								</div>
+								<div class="beginBt">//commencer\\</div>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="thirdStep">
+					<div class="content">
+						<div id="first">
+							<div class="text">
+								?
+								<div class="leftSeparator"></div>
+								<div class="rightSeparator"></div>
+							</div>
+							<div class='title'>
+								<span class="upper">Fille</span> ou <span class="upper">Gar&ccedil;on</span>
+								<div class="choice">
+									<div class="male"></div>
+									<div class="separator"></div>
+									<div class="female"></div>
+								</div>
+							</div>
 
+							<div class="text">
+								?
+								<div class="leftSeparator"></div>
+								<div class="rightSeparator"></div>
+							</div>
+						</div>
+						<div id="second">
+							<div class="text">
+								?
+								<div class="leftSeparator"></div>
+								<div class="rightSeparator"></div>
+							</div>
+							<div class='title'>
+								<div class="upper big">Comment</div>
+								<div class="upper">t'appelles tu</div>
+								<div class="choice">
+									<div class="male"></div>
+									<div class="separator"></div>
+									<div class="female"></div>
+								</div>
+							</div>
+							<div class="text">
+								?
+								<div class="leftSeparator"></div>
+								<div class="rightSeparator"></div>
+							</div>
+							<div class="form">
+								<form action="/getPhrase.php">
+									<input type="text" name="prenom" placeholder="PRENOM" autocomplete="off"></input>
+									<input type="submit" name="valider" value="//VALIDER//" />
+								</form>
+							</div>
+						</div>
+						<div id="final">
+							<div id="postit" class="bleu">
+
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-
-			<div id="separatorBlock"></div>
-
-			<div id="rightBlock">
-				<div class="content">
-					<div class="firstStep">
-						<div class="text">Tu veux tailler tes amis?</div>
-						<div class="img"></div>
-					</div>
-					<div class="secondStep">
-						<div class="text">
-							Comment s'appelle ton pote
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="clear"></div>
+			<footer id="homeFooter">
+				<div id="team">Concept et r&eacute;alisation : <a href="">3X3</a> & <a href="">AGDMAG</a></div>
+			</footer>
 		</div>
-
 	</body>
 </html>
--- www/js/main.js
+++ www/js/main.js
@@ -1,63 +1,114 @@
-function centerElement(element){
-	var pos =  element.position();
-	element.css({"left": pos.left - element.width()/2})
-}
-
-function init() {
-	centerElement($("#logo"));
-
-	//events
-	initBlock($("#leftBlock"), $("#rightBlock"), 0);
-	initBlock($("#rightBlock"), $("#leftBlock"), 1);
-}
-
-var blockClick = false;
-
-function initBlock(block, depBlock, insultes) {
-
-	block.click(function(){
-		if (blockClick) {
-			return;
-		}
-		blockClick = true;
-		var pos = block.position();
-		block.children(".content").children(".firstStep").hide();
-		$(depBlock).css('display', 'none');
-		$("#separatorBlock").hide();
-		block.transition({width: "100%"}, 1000, function(){
-			$(this).children(".content").children(".secondStep").show();
-		});
-		$("#logo").transition({opacity: 0}, 500);
-	});
-
-	initForm(block, insultes);
-}
-
-
-function initForm(block, insultes) {
-	var form = block.find('.secondStep form').submit(function(){
-		var form = $(this);
-		var prenom = form.find('input[name=prenom]').val();
-		var url = form.attr('action');
-		var datas = {prenom : prenom, insultes: insultes};
-
-		$.ajax({
-			url: url,
-			data : datas,
-			type: "POST",
-			dataType: "json"
-		}).success(function(response){
-			alert(response.phrase);
-		}).error(function(){
-		});
-		//interromp le submit
-		return false;
-
-	});
-	console.log(form);
-}
-
 $(document).ready(function(){
-	init();
-	//$("#leftBlock").click();
+	initElements();
+	initEvents();
 });
+
+function initElements(){
+	var header = $('header#homeHeader #logo');
+	var pos = header.position();
+	header.css('top', $(window).height()/2- header.height()/2);
+}
+
+function initEvents(){
+	$(window).oneTime("1s", openHome);
+	$('#leftBlock .beginBt').click(leftClick);
+	$('#rightBlock .beginBt').click(rightClick);
+	$('.thirdStep .choice .male').click(function(){sexeChoice("homme")});
+	$('.thirdStep .choice .female').click(function(){sexeChoice("femme")});
+	$('.thirdStep #second .form form').submit(validName);
+}
+
+function openHome(){
+	$('.firstStep').transition({opacity : 0}, function(){
+		$(this).remove();
+		var time = 1000;
+		$('header#homeHeader #logo').transition({top: 0, position: "fixe"}, time);
+		$('header#homeHeader').transition({position: "fixe", height: 103}, time, function(){
+			$(this).css('border-top', 'solid 2px white');
+			$(this).css('border-bottom', 'solid 2px white');
+		});
+		$('footer#homeFooter').transition({opacity : 1}, time);
+
+	});
+}
+
+function leftClick(){
+	var time = 1000;
+	var el = $('.secondStep #leftBlock');
+	el.css('z-index', 100);
+	el.find('.contentBlock').transition({opacity : 0}, time);
+	el.transition({"margin-left": 0}, time, function(){
+		$('.thirdStep').css('display', 'block').addClass('lightBg');
+		$('.secondStep').remove();
+	});
+	$('.secondStep #rightBlock').transition({"margin-left" : "100%"}, time);
+	$(document).data('insulte', false);
+}
+
+function rightClick(){
+	var time = 1000;
+	var el = $('.secondStep #rightBlock');
+	el.css('z-index', 100);
+	el.find('.contentBlock').transition({opacity : 0}, time);
+		$('.secondStep #leftBlock').transition({"margin-left": "-100%"}, time);
+		el.transition({"margin-left": 0}, time, function(){
+		$('.thirdStep').css('display', 'block').addClass('darkBg');
+		$('.secondStep').remove();
+	});
+	$(document).data('insulte', true);
+}
+
+
+function sexeChoice(sexe) {
+	var time = 1000;
+	$(document).data('sexe', sexe);
+	$('.thirdStep #first').transition({opacity : 0}, time, function(){
+		$(this).remove();
+		$('.thirdStep #second').show(0).transition({opacity : 1}, time);
+	})
+
+}
+
+function validName(){
+	var form = $(this);
+	var prenom = form.find("input[name=prenom]").val();
+	var url = form.attr('action');
+	var datas = new Object;
+	datas.prenom = prenom,
+	datas.sexe = $(document).data('sexe');
+	datas.insulte = $(document).data('insulte');
+	$.ajax({
+		url: url,
+		type: "POST",
+		data : datas,
+		dataType: "json",
+	}).success(function(response){
+		$.extend(datas, response);
+		drawFinal(datas);
+		return false;
+	});
+
+	return false;
+}
+
+
+function drawFinal(datas)
+{
+	var time = 1000;
+	console.log(datas);
+	var prenom = datas.prenom;
+	var phrase = datas.phrase;
+	var color = datas.color;
+	var link = datas.link;
+
+	var finalStr = new String(phrase);
+	finalStr = finalStr.replace("{prenom}", '<span class="prenom">'+prenom+'</span>');
+	$(".thirdStep #final #postit").html(finalStr);
+	console.log(finalStr);
+	$(".thirdStep #second").transition({opacity : 0}, time, function() {
+		$(this).remove();
+		$(".thirdStep #final").css({opacity:0}).show(0);
+		$(".thirdStep #final").transition({opacity : 1});
+	});
+
+}
