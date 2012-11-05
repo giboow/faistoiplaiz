71c2884a02f42432cc6bde845ef88c33dcac3892
giboow <philippe.gibert@gmail.com>
2012-11-05 23:45:58 +0100

    add js libs

--- www/index.php
+++ www/index.php
@@ -5,10 +5,10 @@
 
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-		<script type="text/javascript" src="js/jquery.transit.js"></script>
-		<script type="text/javascript" src="http://jquery.offput.ca/js/jquery.timers.js"></script>
-		<script type="text/javascript" src="js/main.js"></script>
+		<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
+		<script type="text/javascript" src="/js/jquery.transit.js"></script>
+		<script type="text/javascript" src="/js/jquery.timers.js"></script>
+		<script type="text/javascript" src="/js/main.js"></script>
 	</head>
 	<body>
 		<div id="site">
