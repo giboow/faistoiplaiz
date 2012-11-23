edb1836f10ce300adee88be7dfa6cfae9e8e7e11
giboow <philippe.gibert@gmail.com>
2012-11-23 09:21:06 +0100

    ajout tag analytics

--- www/index.php
+++ www/index.php
@@ -169,5 +169,17 @@
 				<div class="fb-like" data-href="https://www.facebook.com/pages/AGD-MAG/137191636324924" data-send="false" data-layout="button_count" data-width="50" data-show-faces="false" data-font="arial"></div><div id="team">Concept et r&eacute;alisation : <a href="">3X3</a> & <a href="">AGDMAG</a></div>
 			</footer>
 		</div>
+		<script type="text/javascript">
+		  //TAG ANALYTICS
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-36281517-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</body>
 </html>
