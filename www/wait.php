c3814cee5db4d8e54b37e6c462dcbbb363b8a083
giboow <philippe.gibert@gmail.com>
2012-11-27 10:35:12 +0100

    corrections

--- www/wait.php
+++ www/wait.php
@@ -3,8 +3,8 @@
 	<head>
 		<title>Fais toi plaiz!</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+		<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
 		<script type="text/javascript" src="/js/jquery.transit.js"></script>
 		<script type="text/javascript" src="/js/jquery.timers.js"></script>
 		<script type="text/javascript">
@@ -99,7 +99,7 @@
 					<div class="bottom">
 						<span id="minutes">
 							<span class="value"></span>
-							Minute
+							Minutes
 						</span>
 						<span id="secondes">
 							<span class="value"></span>
@@ -109,6 +109,17 @@
 				</div>
 			</div>
 		</div>
-	</body>
+		<script type="text/javascript">
+		  //TAG ANALYTICS
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-36281517-1']);
+		  _gaq.push(['_trackPageview']);
 
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+	</body>
 </html>
