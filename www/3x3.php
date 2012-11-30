4a6c13719a91ff107dec87297014fbeddba45266
giboow <philippe.gibert@gmail.com>
2012-11-30 01:06:52 +0100

    Ajout des photos team

--- www/3x3.php
+++ www/3x3.php
@@ -16,11 +16,14 @@
 		$(document).ready(function(){
 			$('header#homeHeader .imgLogo').click(function(){$(document).attr('location', '/');});
 			$('#team .member').hover(function(){
-				$(this).find('.text').stop().animate({"margin-top": "120%"}, 800);
+				$(this).find('.text').stop().animate({"margin-top": "110%"}, 800);
 			}, function(){
 				$(this).find('.text').stop().animate({"margin-top": "200%"}, 800);
 			});
 			$(window).resize(resize);
+			$('.member .reseaux div').click(function(){
+				window.open($(this).attr('rel'));
+			})
 			resize();
 		});
 
@@ -75,36 +78,71 @@
 						<div class="clear"></div>
 					</div>
 					<div id="team">
-						<div class="member">
+						<div class="member papoul">
 							<div class="text">
+								<div class="title">JC PAPOUL</div>
+								<div class="profession">Concepteur-R&eacute;dacteur </div>
+								<div class="prez">"Troubadour des mots &agrave; temps plein,  sinon oui, Papoul c&apos;est mon vrai nom"</div>
+								<div class="reseaux">
+									<div class="facebook" rel="https://www.facebook.com/jeanchristophe.papoul"></div>
+									<div class="twitter" rel="https://twitter.com/Papoul"></div>
+									<div class="clear"></div>
+								</div>
 							</div>
 						</div>
-						<div class="member">
+						<div class="member phil">
 							<div class="text">
 								<div class="title">Philippe Gibert</div>
-								<div class="profession">Ing&eacute;nieur Recherche &amp; D&eacute;veloppement Web</div>
+								<div class="profession">Ing&eacute;nieur R&amp;D Web</div>
 								<div class="prez">"Mes logiciels n&apos;ont jamais de bug. Ils d&eacute;veloppent juste certaines fonctions al&eacute;atoires."</div>
-								<div class="reseaux"></div>
+								<div class="reseaux">
+									<div class="facebook" rel="https://www.facebook.com/philippe.gibert"></div>
+									<div class="twitter" rel="http://twitter.com/giboow"></div>
+									<div class="clear"></div>
+								</div>
 							</div>
 						</div>
-						<div class="member">
+						<div class="member john">
 							<div class="text">
 								<div class="title">John Eber</div>
-								<div class="profession">Directeur Artistique chez The Branch Nation &amp; Fondateur d'AGDMAG</div>
+								<div class="profession">Directeur Artistique &amp; Fondateur d'AGDMAG</div>
 								<div class="prez">"J'aime le cousous, les pates &agrave; la carbonara, mais aussi la cr&eacute;a.."</div>
+								<div class="reseaux">
+									<div class="facebook" rel="https://www.facebook.com/john.eber.7?ref=tn_tnmn"></div>
+									<div class="twitter" rel="https://twitter.com/AGDMAG"></div>
+									<div class="clear"></div>
+								</div>
+							</div>
+						</div>
+						<div class="member anais">
+							<div class="text">
+								<div class="title">Anais Duhau-Duba</div>
+								<div class="profession">Chef de projet digital</div>
+								<div class="prez">"Cascadeuse professionnelle, je d&eacute;vale les escaliers de l&apos;agence comme personne"</div>
+								<div class="reseaux">
+									<div class="facebook" rel="https://www.facebook.com/john.eber.7?ref=tn_tnmn"></div>
+									<div class="twitter" rel="https://twitter.com/AGDMAG"></div>
+									<div class="clear"></div>
+								</div>
+							</div>
+						</div>
+						<div class="member marie-eva">
+							<div class="text">
+								<div class="title">Marie-Eva Gatuingt</div>
+								<div class="profession">Chef de projet digital</div>
+								<div class="prez">"Collectionne les catbread, trouve la vie plus belle en instagram, mais regrette le minitel"</div>
+								<div class="reseaux">
+									<div class="facebook" rel="https://www.facebook.com/anais.duhau"></div>
+									<div class="clear"></div>
+								</div>
+							</div>
+						</div>
+						<div class="member joanne">
+							<div class="text">
+								<div class="title">Joanne Dunoyer</div>
+								<div class="profession">Directrice de client&egrave;le</div>
+								<div class="prez"></div>
 								<div class="reseaux"></div>
-							</div>
-						</div>
-						<div class="member">
-							<div class="text">
-							</div>
-						</div>
-						<div class="member">
-							<div class="text">
-							</div>
-						</div>
-						<div class="member">
-							<div class="text">
 							</div>
 						</div>
 						<div class="clear"></div>
